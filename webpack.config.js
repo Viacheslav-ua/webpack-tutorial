2290f062feedfeb638c57b7143efe468ee8bcd76
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-07-31 19:41:44 +0300

    настройки wp

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+//const { PassThrough } = require('stream');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, "./src/index.js"),
+    },
+
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].bundle.js"
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+};
